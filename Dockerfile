e18d836395bb26f43a1ce41c87116f5166cb3852
C. Guychard <christophe@certificare.fr>
2020-12-07 17:32:52 +0100

    [ADD] first commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM article714/debian-based-container:latest
+LABEL maintainer="C. Guychard <Article714>"
+
+
+
+# Generate locale C.UTF-8 for postgres and general locale data
+ENV LANG C.UTF-8
+
+# Container tooling
+
+COPY container /container
+
+# container building
+
+RUN /container/build.sh
+
+# port exposed (default)
+
+EXPOSE 80
+EXPOSE 443
---
+++ container/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#set -x
+
+apt-get update
+
+# Generate French locales
+localedef -i fr_FR -c -f UTF-8 -A /usr/share/locale/locale.alias fr_FR.UTF-8
+
+export LANG=en_US.utf8
+
+# Install basic needed packages
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -qy --no-install-recommends wget gnupg ca-certificates runit rsyslog logrotate
+
+# Install HaProxy from pre-built packages
+curl https://haproxy.debian.net/bernat.debian.org.gpg | apt-key add -
+echo deb http://haproxy.debian.net buster-backports-2.2 main | tee /etc/apt/sources.list.d/haproxy.list
+
+apt-get update
+apt-get upgrade -yq
+
+apt-get install haproxy=2.2.\*
+
+#--
+# Cleaning
+
+apt-get -yq clean
+apt-get -yq autoremove
+rm -rf /var/lib/apt/lists/*
+rm -rf /tmp/ci
+rm -f tmp/*_dependencies.txt
