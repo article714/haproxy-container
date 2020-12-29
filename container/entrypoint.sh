bbaae5167fd53e71582f9462b79511d78f497a91
C. Guychard <christophe@certificare.fr>
2020-12-29 16:37:14 +0100

    [FIX] wrong entrypoint

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -5,10 +5,10 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin
 #set -x
 
 start() {
-    if [[ ! -f "/container/ssl/00_haproxy.crt" ]]; then
+    if [[ ! -f "/container/config/ssl/00_haproxy.crt" ]]; then
         # Build self-signed Certificate
-        chown slapd.root /container/config/slapd/certs
-        cd /container/ssl
+        chown haproxy.root /container/ssl
+        cd /container/config/ssl
         chpst -u haproxy openssl req -newkey rsa:4096 -days 1001 -nodes -x509 -subj "/C=FR/O=Article714/CN=haproxy.localhost" -keyout "00_haproxy.key" -out "00_haproxy.crt"
         cd /
     fi
