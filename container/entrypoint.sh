5cc7098921548166d410c5424acf7fcc82279420
C. Guychard <christophe@certificare.fr>
2020-12-29 15:32:48 +0100

    [ADD] basic ssl cert generated on start

---
+++ container/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+
+#set -x
+
+start() {
+    if [[ ! -f "/container/ssl/00_haproxy.crt" ]]; then
+        # Build self-signed Certificate
+        chown slapd.root /container/config/slapd/certs
+        cd /container/ssl
+        chpst -u haproxy openssl req -newkey rsa:4096 -days 1001 -nodes -x509 -subj "/C=FR/O=Article714/CN=haproxy.localhost" -keyout "00_haproxy.key" -out "00_haproxy.crt"
+        cd /
+    fi
+
+    exec runsvdir -P /container/services
+}
+
+case "$1" in
+start)
+    start
+    ;;
+shell)
+    exec "/bin/bash"
+    ;;
+--)
+    start
+    ;;
+*)
+    exec "$@"
+    ;;
+esac
+
+exit 1
